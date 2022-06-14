fcba5f6119164734cbe290cf784fb12c54a6b1b4
Ryan Struber <rstruber@users.noreply.github.com>
2022-06-14 15:22:18 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+basic.show_string("welcome to outer space")
+basic.pause(100)
+music.play_melody("B A G A G F A C ", 120)
+basic.show_string("Welcome earthling")
+for index in range(4):
+    music.play_melody("C D C G - C5 E A ", 120)
+basic.show_string("Hello I come in peace")
---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showString("welcome to outer space")
+basic.pause(1000)
+music.playMelody("B A G A G F A C ", 120)
+basic.showString("Welcome earthling")
+for (let index = 0; index < 4; index++) {
+    music.playMelody("C D C G - C5 E A ", 119)
+}
+basic.showString("Hello I come in peace")
+music.playMelody("A - C5 E D F A B ", 120)
